f08435f42e450846695746ff60a07697990fabf9
jainpulkit22 <18ucs163@lnmiit.ac.in>
2021-04-05 12:15:21 +0530

    Port changed

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.use(function(req, res, next){
 app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.listen(3000, function(){
+app.listen(process.env.PORT||3000, function(){
     console.log("YelpCamp has started")
 })
